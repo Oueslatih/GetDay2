5d5268019a51c8167488f49a183f2196458d04e9
zdoue <zdoue@10.0.0.198>
2022-08-02 22:59:41 -0400

    commit us 789

---
+++ src/Demo/demoTest.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class demoTest {
+
+	
+	// for 456
+	public static void divide( int a, int b) {
+		
+		System.out.println(a/b);
+	}
+	
+	  // for us789
+       public static void Demo() {
+    	   System.out.println("Demo.....");
+       }
+}
